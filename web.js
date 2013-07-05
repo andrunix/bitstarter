5801ba14cd6fef6583061ca593ca96724adffff6
Andrew Pierce <andrunix@gmail.com>
2013-07-05 19:44:45 +0000

    Updated to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var text = fs.readFileSync('index.html');
+    var s = text.toString();
+    response.send(s);
 });
 
 var port = process.env.PORT || 5000;
